a5018093729a4fa2daa82993d3382f417936cd7b
Hack Reactor Student <student@hackreactor.com>
2017-03-29 16:27:10 -0700

    Complete stack and queueu pseudoclassical class implementations

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  
 };
+
+
+Queue.prototype = {
+  size : function() {
+    count = 0;
+    for(var key in this.storage){
+    	count++
+    }
+    return count;
+  },
+	enqueue : function(value) {
+    tempStorage = {};
+    if (this.size() > 0) {
+     for(var key in this.storage){
+        tempStorage[Number(key) + 1] = this.storage[key];
+      } 
+      this.storage = tempStorage; 
+    }
+    this.storage[0] = value;    
+  },
+
+  dequeue : function() {
+    var last = this.storage[this.size()-1];    
+    delete this.storage[this.size()-1];
+    console.log(this.storage);
+    return last;
+  }
+};
+
+Queue.prototype.constructor = Queue;
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Use an object with numeric keys to store values
+  
+  this.storage = {};
 };
 
 
+Stack.prototype = {
+	size : function() {
+		var count = 0;
+		for (var key in this.storage) {
+			count++;
+		}
+     	return count;
+  	}, 
+
+	push : function(value) { 
+      this.storage[this.size()] = value;
+  	},
+
+  pop : function() {
+    var last = this.storage[this.size()-1];
+    delete this.storage[this.size()-1];
+    return last;
+  }
+};
+
+Stack.prototype.constructor = Stack;
