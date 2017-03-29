71af49333e7226f0e3abda0e22d977d9409fe088
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:22:00 -0700

    Completed The functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,35 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    tempStorage = {};
+    if (count > 0) {
+     for(var key in someInstance.storage){
+        tempStorage[Number(key) + 1] = someInstance.storage[key];
+      } 
+      someInstance.storage = tempStorage; 
+    }
+    someInstance.storage[0] = value;    
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var last = someInstance.storage[count-1];    
+    delete someInstance.storage[count-1];
+    console.log(someInstance.storage);
+    if (count > 0) {
+      count--;
+    }
+    return last;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  var count = 0;
 
   // Implement the methods below
-  someInstance.push = function(value) {
-  };
+  someInstance.push = function(value) { 
+    someInstance.storage[count] = value;
+    count++;
+  };;
 
   someInstance.pop = function() {
+    var last = someInstance.storage[count-1];
+    delete someInstance.storage[count-1];
+    if(count > 0){
+      count--;
+    }
+    return last;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
