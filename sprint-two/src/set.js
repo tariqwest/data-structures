7d544b5ad7f054f523c368a1c988117928b50b14
Hack Reactor Student <student@hackreactor.com>
2017-03-30 16:11:19 -0700

    Complete set implementation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,37 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  
+  set._storage = {}; // fix me
+  
   return set;
+
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	// See if key exists
+	// If not exist, add it
+	if(!this.contains(item)){
+		this._storage[item] = item;
+	}
 };
 
 setPrototype.contains = function(item) {
+	// See if key exists
+	// If exists, return true
+	if(this._storage.hasOwnProperty(item)){
+		return true;
+	}
+	return false;
 };
 
 setPrototype.remove = function(item) {
+	// See if key exists
+	// If exists, remove
+	if(this.contains(item)){
+		delete this._storage[item];
+	}
 };
 
 /*
