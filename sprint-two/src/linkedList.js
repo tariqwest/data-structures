f629496f7ae5f7f503a825e933151d16842b0887
alexitaylor <alexi@bluesunrise.com>
2017-03-30 08:08:30 -0700

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,53 +1,67 @@
 var LinkedList = function() {
-  var list = {};
-  list.head = null; // A node that no other nodes refer to
-  list.tail = null; // A node that doesn't refer to any other nodes
-  var count = 1;
+    var list = {};
+    list.head = null;
+    list.tail = null;
+    var count = 1;
 
-// list = { head: {value:22, link: 1}, 1: {value: 33, link: 1}, 2: {value: 44, link: 3}, 3{value 55, link: tail}, tail{...}}
-//{head: {value:22 , next: 33}, node: {value: 33; next: tail}, tail{...}}
-  list.addToTail = function(value) {
-    // Create node
-    if (list.tail === null) {
-      list.tail = {value: value, link: null};
-    }else if(list.head === null){
-      list.head = list.tail;
-      list.head.link = 'tail';
-      list.tail = {value: value, link: null};
-    }else{     
-      var temp = list.tail;
-      list.tail = {value: value, link: null};
-      temp.link = 'tail';
-      
-      //head.link = count;
-      list[count] = temp;
-      count++;
-    }
-  };
+    //{ tail: {value: 44, next: null}}
 
-  list.removeHead = function() {
-    var nextKey = list.head.link;
-    var next = list[nextKey];
-    list.head = next;
-    delete list[next];
-  };
+    list.addToTail = function(value) {
+        if (list.tail === null) {
+            list.tail = Node(value);
+        } else if (list.head === null) {
+            list.head = list.tail;
+            list.head.next = 'tail';
+            list.tail = Node(value);
+        } else {
+            var tempNode = list.tail;
+            list.tail = Node(value);
+            tempNode.next = 'tail';
+            count === 1 ? list.head.next = count : list[count - 1].next = count;
+            list[count] = tempNode;
+            count++;
+        }
+    };
 
-  list.contains = function(target) {
-    // Look at each item in list
-      // Return true if targte found
-      // Return false if target not found
-  };
+    list.removeHead = function() {
+        if (list.head === null) {
+            var tailValue = list.tail.value;
+            delete list.tail;
+            return tailValue;
+        }
+        var nextKey = list.head.next;
+        var next = list[nextKey];
+        var oldValue = list.head.value;
 
-  return list;
+        if (nextKey === 'tail') {
+            list.head = list.tail;
+            delete list.tail;
+        } else {
+            list.head = next;
+            delete list[nextKey];
+        }
+        return oldValue;
+    };
+
+    list.contains = function(target) {
+        for (var key in list) {
+            if (list[key].value === target) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    return list;
 };
 
 var Node = function(value) {
-  var node = {};
+    var node = {};
 
-  node.value = value;
-  node.next = null;
+    node.value = value;
+    node.next = null;
 
-  return node;
+    return node;
 };
 
 /*
