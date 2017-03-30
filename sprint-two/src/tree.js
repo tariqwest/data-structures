ae8368a153858b050710e30b83b0abc0fc024e70
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:47:54 -0700

    Complete tree implemintation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,11 +13,23 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
-};
 
+	if (this.value === target) {
+		return true;
+	}
+	if(this.children.length > 0){
+		for (var i = 0; i < this.children.length; i++) {
+				if(this.children[i].contains(target)){
+					return true;
+				}
+		}
+	}
+	return false;
+};
 
 
 /*
