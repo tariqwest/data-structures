e4ca566c221631187b91ba33d7a9cfe572aee1eb
alexitaylor <alexi@bluesunrise.com>
2017-03-31 07:11:07 -0700

    Complete hash table implementation
    All test pass. Each storage array holds multiple key-value pairs.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,30 +1,53 @@
-
-
 var HashTable = function() {
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+    this._limit = 8;
+    this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
-  	var index = getIndexBelowMaxForKey(k, this._limit);
-  	this._storage.set(index, v);
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    var keyValuePair = [k, v];
+    var storageSlot = this._storage.get(index);
+    if (storageSlot) {
+        var overwrite = false;
+        for (var i = 0; i < storageSlot.length; i++) {
+            if (storageSlot[i][0] === k) {
+                storageSlot[i][1] = v;
+                overwrite = true;
+            }
+        }
+        if (overwrite === false) {
+            storageSlot.push(keyValuePair);
+        }
+    } else {
+        storageSlot = [];
+        storageSlot.push(keyValuePair);
+        this._storage.set(index, storageSlot);
+    }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    var retrievedValue = this._storage.get(index);
+    for (var i = 0; i < retrievedValue.length; i++) {
+        if (retrievedValue[i][0] === k) {
+            return retrievedValue[i][1];
+        }
+    }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index);
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    //this._storage.set(index);
+    var storageSlot = this._storage.get(index);
+    if (storageSlot) {
+        for (var i = 0; i < storageSlot.length; i++) {
+            if (storageSlot[i][0] === k) {
+                storageSlot[i].splice(i, 1);
+            }
+        }
+    }
 };
-
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
