012458bc02914bca928547f190f64104fdc7780c
Tariq West <tariqwest@gmail.com>
2017-03-30 21:34:43 -0700

    Complete hashtable basic implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,18 +6,48 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  	var index = getIndexBelowMaxForKey(k, this._limit);
-  	this._storage.set(index, v);
+    var index = getIndexBelowMaxForKey(k, this._limit);
+    var keyValuePair = [k,v]
+    var storageSlot = this._storage.get(index);
+    if(storageSlot){
+      var keyValuePairExists = false;
+      for(var i = 0; i<storageSlot.length; i++){
+        if(storageSlot[i][0] === k){
+          storageSlot[i][1] = v;
+          keyValuePairExists = true;
+        }
+      }  
+      if(keyValuePairExists === false){
+        storageSlot.push(keyValuePair);
+      }
+    }else{
+      storageSlot = [];
+      storageSlot.push(keyValuePair);
+      this._storage.set(index, storageSlot);
+    }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var storageSlot = this._storage.get(index);
+  for(var i=0; i<storageSlot.length; i++){
+    if(storageSlot[i][0] === k){
+      return storageSlot[i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index);
+  var storageSlot = this._storage.get(index);
+  if(storageSlot){
+    for(var i=0; i<storageSlot.length; i++){
+      if(storageSlot[i][0] === k){
+        storageSlot.splice(storageSlot.indexOf(storageSlot[i]), 1);
+      }
+    }
+  }
 };
 
 
